8169963775adc91f64e35ee0679b6dd5d7b66d42
Aditya-naryal <134803330+Aditya-naryal@users.noreply.github.com>
2025-12-23 00:15:49 +0530

    Create CommunityWallet.sol
    created a smart contract for community wallet

---
+++ CommunityWallet.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+contract CommunityWallet {
+    address owner;
+    uint approveThreshold;
+
+    constructor() {
+        owner = msg.sender;//because owner is going to be the one who is deploying the contract not user input
+        approveThreshold =3; 
+    } 
+
+    uint256 contractBalance;
+    uint256 spendingRequestId;// counter used for both mappings
+    mapping(address => uint256) addressToBalance;
+    struct SpendingRequest {
+        string description;
+        uint amount;
+        address recipient;
+        bool isExecuted;
+        uint approvalCount;
+    }
+    mapping(uint256 => SpendingRequest) IdToRequest;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "User can't access this function");
+        _; //is used to define the end of modifier and beginning of function call written next to it
+    }
+
+    function ethDeposit() public payable {
+        // now we dont have to override the function using parameter variable like owner or value 
+        // because we already have internal variable and they change every time the function is call
+        // thats why we dont use owner even though it is assigned as msg.sender in constructor
+        require(msg.value != 0, "You have Zero ETH");
+        addressToBalance[msg.sender] += msg.value;
+        contractBalance += msg.value;       
+    }
+    
+    function createSpendingRequest(string memory _description, uint _amount, address _recipient) public onlyOwner {
+        require (_amount>0 && _recipient != address(0), "Invalid Input");
+        // address(0) defines the address starting with zero  like 0000000000000..
+        IdToRequest[spendingRequestId] = SpendingRequest({
+            description: _description,
+            amount: _amount,
+            recipient: _recipient,
+            isExecuted: false,
+            approvalCount: 0
+        });
+        spendingRequestId ++; 
+        //dont need to wait for the approval because 
+        //we have to assign id at creation thats why increment it here
+    }
+
+    mapping(uint => mapping(address => bool)) requestIdToApprovals; // to store whic user has approve which spending request
+    function approveSpendingRequest(uint _spendingRequestId) public {
+        require(_spendingRequestId < spendingRequestId, "Id doesn't exist");
+        require(addressToBalance[msg.sender] > 0, "insufficient balance");
+        require(IdToRequest[_spendingRequestId].isExecuted == false, "request is already executed");
+        require(requestIdToApprovals[_spendingRequestId][msg.sender] == false,"you already approved this tx");
+        IdToRequest[_spendingRequestId].approvalCount ++;
+        requestIdToApprovals[_spendingRequestId][msg.sender] = true;
+    }
+
+    function executeSpendingRequest(uint _spendingRequestId) public {
+        require(_spendingRequestId < spendingRequestId, "Id doesn't exist");
+        require(IdToRequest[_spendingRequestId].isExecuted == false, "request is already executed");
+        require(IdToRequest[_spendingRequestId].approvalCount >= approveThreshold, "Not approved yet");
+        require(address(this).balance >= IdToRequest[_spendingRequestId].amount, "not enough balance");
+        IdToRequest[_spendingRequestId].isExecuted = true;
+        (bool success, ) = IdToRequest[_spendingRequestId].recipient.call{value: IdToRequest[_spendingRequestId].amount}("");
+        require(success, "ETH transfer failed");
+    }   
+
+}
